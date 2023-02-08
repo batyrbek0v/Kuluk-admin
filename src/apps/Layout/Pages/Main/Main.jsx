86829d205a03faf5b4282999bc4f49d47d36e28d
Эрлан Камбаров <erlankambarov94@gmail.com>
2023-02-08 13:17:53 +0600

    added Header for main, also testing vercel app

--- src/apps/Layout/Pages/Main/Main.jsx
+++ src/apps/Layout/Pages/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Header } from '../../../../components/Header/Header'
 import { Title } from '../../../../components/Title/Title'
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
   return (
     <React.Fragment>
       <div className="container">
+        <Header previous="Статистика" />
         <Title title={'Статистика'} />
         <div className="container-inner">
 
